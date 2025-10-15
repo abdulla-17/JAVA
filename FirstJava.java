824fc7be2abba54485f929cb1fa921e8f2de7911
abdulla-17 <abd26437@gmail.com>
2025-10-15 15:41:59 +0530

    Add initial Java programs: First, Second, day2hw, and hello_world

---
+++ FirstJava.java
@@ -0,0 +1,12 @@
+class First {
+    public static void main(String[] args) {
+        System.out.println("This is my first Java program!");
+    }
+    
+}
+class Second {
+    public static void main(String[] args) {
+        System.out.println("This is my second Java program!");
+    }
+    
+}
---
+++ day2hw.java
@@ -0,0 +1,13 @@
+class day2hw {
+    public static void main(String[] args) {
+        int qty = 4;
+        double unitprice = 18.75;
+        String item = "Soap";
+        double total_price = qty * unitprice;
+        System.out.println("Item: " + item);
+        System.out.println("Quantity: " + qty);
+        System.out.println("Unit Price: " + unitprice);
+        System.out.println("Total Price: " + total_price);
+    }
+
+}
