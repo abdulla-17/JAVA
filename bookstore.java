a4f4ccbdabd573796da9cad96c9df1b4b384f113
abdulla-17 <abd26437@gmail.com>
2025-10-16 14:33:59 +0530

    Refactor bookstore class to encapsulate discount calculation and output in main method

--- bookstore.java
+++ bookstore.java
@@ -1,7 +1,5 @@
 class bookstore {
-    public static void main(String[] args)
-    {
-        int bookcount = 3;
+     int bookcount = 3;
         int price = 275;
         int total_amount = bookcount * price;
         int discount = 10;
@@ -9,7 +7,11 @@ class bookstore {
         double discount_value = (discount / 100.0) * total_amount;
         double final_total = total_amount - discount_value;
 
-        System.out.println("Discount: " + discount_value);
-        System.out.println("Total after discount: " + final_total);
+    public static void main(String[] args)
+    {
+        bookstore store = new bookstore();
+       
+        System.out.println("Discount: " + store.discount_value);
+        System.out.println("Total after discount: " + store.final_total);
     }
 }
