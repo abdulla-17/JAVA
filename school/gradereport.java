ab12568b35b3bcd97dc0d33cfbb311a712b09e7f
abdulla-17 <abd26437@gmail.com>
2025-10-23 14:26:02 +0530

    Refactor GradeReport class for improved readability and functionality

--- school/gradereport.java
+++ school/gradereport.java
@@ -1,41 +1,28 @@
-package school;
-
-public class gradeReport {
+class GradeReport {
     public static void main(String[] args) {
-        int[] marks = {78, 82, 91, 65, 34};
-
+        int[] marks = { 78, 82, 91, 65, 34 };
         int total = 0;
-        float average;
-        boolean isFail = false;
 
-        for (int mark : marks) {
-            if (mark < 35) {
-                isFail = true;
-                break;  
+        for (int i = 0; i < marks.length; i++) {
+            if (marks[i] < 35) {
+                System.out.println("Result: Fail (Scored below 35 in subject " + (i + 1) + ")");
+                break;
             }
-            total += mark;
+            total += marks[i];
         }
 
-       
-        average = (float) total / marks.length;
-
+        double average = total / (double) marks.length;
         System.out.println("Total Marks: " + total);
         System.out.println("Average Marks: " + average);
 
-        if (isFail) {
-            System.out.println("Result: Fail");
+        if (average >= 90) {
+            System.out.println("Grade: A");
+        } else if (average >= 75) {
+            System.out.println("Grade: B");
+        } else if (average >= 60) {
+            System.out.println("Grade: C");
         } else {
-            System.out.print("Grade: ");
-            if (average >= 90) {
-                System.out.println("A");
-            } else if (average >= 75) {
-                System.out.println("B");
-            } else if (average >= 60) {
-                System.out.println("C");
-            } else {
-                System.out.println("D");
-            }
+            System.out.println("Grade: D");
         }
     }
-}  
-
+}
