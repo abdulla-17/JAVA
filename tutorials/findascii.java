3f6abce8bb46e72bbb5861eb279a541fbd83e7e5
abdulla-17 <abd26437@gmail.com>
2025-11-04 15:04:18 +0530

    Add findascii class to demonstrate ASCII value calculation

---
+++ tutorials/findascii.java
@@ -0,0 +1,12 @@
+package tutorials;
+
+public class findascii {
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+       
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+}
+}
